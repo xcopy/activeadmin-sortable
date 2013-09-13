ef606b340a38f1a440364d506dcb978b8483ca79
Bharat Gupta <bindassbharat311@gmail.com>
2013-09-13 14:07:43 +0530

    Allow for mounting as an engine
    Sort URL generation should be irrespective of the app/engine url_helpers to allow for mounting as an engine- #url_to replaced with #resource_path
    
    fixes #5, fixes #8, fixes #13

--- lib/activeadmin-sortable.rb
+++ lib/activeadmin-sortable.rb
@@ -23,7 +23,7 @@ module ActiveAdmin
 
       def sortable_handle_column
         column '', :class => "activeadmin-sortable" do |resource|
-          sort_url = url_for([:sort, :admin, resource])
+          sort_url = resource_path(resource) + "/sort"
           content_tag :span, HANDLE, :class => 'handle', 'data-sort-url' => sort_url
         end
       end
