77798d61731c1e5af915e19925cdfad5806b0e72
Adam McCrea <adam@adamlogic.com>
2013-10-27 13:01:27 -0400

    WIP - include as-sortable in example app

--- example/app/app/admin/things.rb
+++ example/app/app/admin/things.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Thing do
   
+  config.sort_order = 'position_asc'
+  config.paginate   = false
+
+  sortable
+
+  index do
+    sortable_handle_column
+    column :position
+    column :name
+  end
+
 end
